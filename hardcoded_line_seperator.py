1308944e2ede4a47826532071815a83175754de1
finno20 <47617037+finno20@users.noreply.github.com>
2019-03-20 20:26:02 -0700

    analyze lines data

---
+++ hardcoded_line_seperator.py
@@ -0,0 +1,47 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+file_name =  "data/forms/a01-000x.png"
+
+
+img = cv2.imread(file_name, cv2.IMREAD_GRAYSCALE)
+img = 255-np.array(img)
+#img = img[640:2777]
+#img = cv2.resize(img, (100, 100))
+
+size = img.shape[0]
+maxes = np.max(img, axis=1)
+means = np.mean(img, axis=1)
+mult = maxes*means/30
+sep = mult*1
+sep[sep < 40] = 0
+sep[sep >= 40] = 100
+switch = np.roll(sep, 1)
+switch = switch != sep
+switch = switch.astype(int)*100
+switches = []
+for i in range(switch.shape[0]):
+    if switch[i]:
+        switch[i+1:i+40] = 0
+        switches += [i]
+lines = []
+switches = switches[:2]
+for i in range(len(switches)):
+    pass    
+
+
+print(maxes.shape)
+print(img.shape)
+
+x = np.arange(size)
+
+plt.imshow(img.transpose(), cmap=plt.cm.binary)
+plt.plot(x, maxes)
+plt.plot(x, means)
+plt.plot(x, mult)
+#plt.plot(x, np.sort(mult))
+#plt.plot(x, sep)
+#plt.plot(x, switch)
+plt.show()
+
