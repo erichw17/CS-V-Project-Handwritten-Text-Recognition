f54cd944d8e8e87668164944bc70578dc03be74d
finno20 <47617037+finno20@users.noreply.github.com>
2019-03-21 12:52:41 -0700

    it works now

--- hardcoded_line_seperator.py
+++ hardcoded_line_seperator.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-file_name =  "data/forms/a01-000x.png"
+file_name =  "data/forms/a01-000u.png"
 
 
 img = cv2.imread(file_name, cv2.IMREAD_GRAYSCALE)
@@ -31,8 +31,34 @@ for i in range(len(switches)):
     pass    
 
 
-print(maxes.shape)
-print(img.shape)
+#print(maxes.shape)
+#print(img.shape)
+
+#for i in range(1776, 1960):
+ #   print(str(i) + ": " + str(mult[i]-mult[i-1]))
+avg = np.zeros(mult.shape[0])
+s = 10
+for i in range(s, mult.shape[0]-s):
+    for j in range(i-s, i+s+1):
+        avg[i] += mult[j]
+    avg[i] /= 21
+
+m = 50
+maximums = []
+for i in range(mult.shape[0]):
+    higher = True
+    for j in range(i-m, i+m+1):
+        if j >= 0 and j < mult.shape[0] and j <= j != i and mult[i] < mult[j]:
+            higher = False
+    if higher:
+        maximums += [i]
+
+halves = []
+for i in range(len(maximums)-1):
+    halves += [(maximums[i]+maximums[i+1])/2]
+
+for half in halves:
+    plt.plot((half, half), (0, img.shape[1]), "b")
 
 x = np.arange(size)
 
@@ -40,6 +66,7 @@ plt.imshow(img.transpose(), cmap=plt.cm.binary)
 plt.plot(x, maxes)
 plt.plot(x, means)
 plt.plot(x, mult)
+#plt.plot(x, avg)
 #plt.plot(x, np.sort(mult))
 #plt.plot(x, sep)
 #plt.plot(x, switch)
